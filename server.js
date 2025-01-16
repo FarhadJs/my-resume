1d154ea44dfb7e0653b323e51b19211daaf9fa24
FarhadJs <farhadfallahi2004@gmail.com>
2025-01-16 18:38:38 +0330

    🔧 Update server log message to include protocol in the running port

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.get("*", (req, res) => {
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
